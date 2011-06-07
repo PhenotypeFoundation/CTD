30a89a5f15a0e92d0fc2f5e0132cb43a46aafd25
btvandijk <tjeerd@thehyve.nl>
2011-06-07 11:53:18 +0000

    #53 is resolved by this ticket. Also commenting is added

--- web/scripts/overviewscripts.js
+++ web/scripts/overviewscripts.js
@@ -1,22 +1,38 @@
+// Keep track of the div that is expanded
 var overviewdetail = "";
 
+/**
+ * Function that loads and shows a tbody containing details about an assay
+ */
 function showOverviewDetails(assay) {
     var nowopen = overviewdetail;
+    
+    // Close all open detail tbodies
     closeOverviewDetails();
+    
+    // If nowopen==assay, then the user wanted to close the tbody and not open
+    // another one
     if(nowopen!=assay) {
         overviewdetail = assay;
+        // Show the tbody
         $("#"+overviewdetail+"HiddenRows").show();
         $.ajax({
             url: "./samplesoverview.jsp?assayToken="+assay,
             success: function(data, textStatus, jqXHR){
-                $("#"+overviewdetail+"Details").html(data);
+                // Set the inner HTML of the tbody (a table with data)
+                $("#"+assay+"Details").html(data);
             }
         });
     }
 }
 
+/**
+ * Function that closes a tbody containing details about an assay
+ */
 function closeOverviewDetails() {
+    // If there is a details tbody open
     if(overviewdetail!="") {
+        // Hide the tbody
         $("#"+overviewdetail+"HiddenRows").hide();
         overviewdetail = "";
     }
